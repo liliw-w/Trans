504d2f62b7155a61520720298a8ae90e90f149d6
ll <liliw1@uchicago.edu>
2021-08-17 16:27:03 -0500

    Make code more readable

--- manhattan_file.R
+++ manhattan_file.R
@@ -1,8 +1,14 @@
+rm(list = ls())
+dir_data = "~/xuanyao_llw/DGN_PCO.lambda.01/p/"
+file_signal = "~/xuanyao_llw/DGN_PCO.lambda.01/FDR/signals.chr.module.perm10.FDR10.txt"
+file_indep_signal = "~/xuanyao_llw/DGN_PCO.lambda.01/postanalysis/indep.signals.chr.module.perm10.FDR10.txt"
+file_annotation = "/project2/xuanyao/data/mappability/gencode.v19.annotation.table.txt"
+file_signals_eQTL = "/project2/xuanyao/llw/DGN_PCO.lambda.01/trans_cis/signals_eQTL_FDR10.txt"
+file_signals_sQTL = "/project2/xuanyao/llw/DGN_PCO.lambda.01/trans_cis/signals_sQTL_FDR10.txt"
+
 # prepare gwas data
 {
-  rm(list = ls())
 
-  dir_data = "./p/"
   chr_seq = 1:22
 
 
@@ -28,35 +34,146 @@
 
 
 
-
 # prepare signals and pvalues
 {
-    PCO_trans = read.table("signals.chr.module.perm10.txt", header = FALSE, col.names = c("module.snp", "pvalue", "qvalue"), row.names = 1, stringsAsFactors = FALSE)
-    PCO_trans$SNP = sapply(strsplit(rownames(PCO_trans), ":"), function(x) paste(x[[2]], x[[3]], sep = ":"))
+  require(data.table)
 
-    PCO_trans_uniq = NULL
-    for(i in unique(PCO_trans$SNP)){
-      tmp = PCO_trans[PCO_trans$SNP == i, ]
-      PCO_trans_uniq = rbind(PCO_trans_uniq, tmp[which.min(tmp$pvalue), ])
+  PCO_trans = read.table(file_signal, header = FALSE, col.names = c("module.snp", "pvalue", "qvalue"), row.names = 1, stringsAsFactors = FALSE)
+  PCO_trans$SNP = sapply(strsplit(rownames(PCO_trans), ":"), function(x) paste(x[[2]], x[[3]], sep = ":"))
+
+  PCO_trans_uniq = NULL
+  for(i in unique(PCO_trans$SNP)){
+    tmp = PCO_trans[PCO_trans$SNP == i, ]
+    PCO_trans_uniq = rbind(PCO_trans_uniq, tmp[which.min(tmp$pvalue), ])
+  }
+
+  # prepare annotation files
+  gene_pos = fread(paste0(file_annotation),
+                   sep = '\t', header = T)
+  gene_pos = as.data.frame(gene_pos[gene_pos$Class %in% c('protein_coding'), ]) #, 'lincRNA'
+
+
+  PCO_trans_uniq$PCO = PCO_trans_uniq$SNP
+  PCO_trans_uniq$elife = "no"
+  PCO_trans_uniq$both = "no"
+  PCO_trans_uniq$neargene = "NA"
+  for(i in 1:nrow(PCO_trans_uniq)){
+
+    chr = as.numeric(strsplit(PCO_trans_uniq$SNP[i], ":")[[1]][1])
+    snpos = as.numeric(strsplit(PCO_trans_uniq$SNP[i], ":")[[1]][2])
+
+    {
+      ind2 = gene_pos$Chromosome == paste0("chr", chr) & abs(gene_pos$Start - snpos)<5*10^5
+      dis_snp_gene2 = gene_pos[ind2, "Start"]-snpos
+      tmp2 = cbind(gene_pos[ind2, ], 'dis_snp_gene' = dis_snp_gene2)
+
+      if(nrow(tmp2)>0){
+        PCO_trans_uniq[i, "neargene"] = tmp2[order(abs(tmp2$dis_snp_gene)), ][1, "GeneSymbol"]
+      }
     }
+  }
 
-    write.table(PCO_trans_uniq, file = "PCO.trans.uniq.txt", quote = FALSE)
+  write.table(PCO_trans_uniq, file = "PCO.trans.uniq.txt", quote = FALSE)
+}
+
+
+# plot manhattan plot
+library(qqman)
+library(ggrepel)
+library(dplyr)
+
+
+PCO_trans_uniq[PCO_trans_uniq$pvalue==0, "pvalue"] = 1e-18 #pvalues<1e-17 is displayed as 0
+gwasResults$P = 1; gwasResults[match(PCO_trans_uniq$SNP, gwasResults$SNP), "P"] = PCO_trans_uniq$pvalue
+#gwasResults = readRDS("manhattan.rds")
+#PCO_trans_uniq = read.table(file = "PCO.trans.uniq.txt", stringsAsFactors = FALSE, row.names = 1, header = T)
+#PCO_trans_indep = read.table("PCO.trans.indep.txt", header = TRUE, stringsAsFactors = FALSE)
+#PCO_trans_indep[25, 'neargene']='IKZF1'
+{
+
+  signals_eQTL = as.character(read.table(file_signals_eQTL, stringsAsFactors = FALSE)[[1]])
+  signals_sQTL = as.character(read.table(file_signals_sQTL, stringsAsFactors = FALSE)[[1]])
+
+  anno_dat = PCO_trans_uniq %>% group_by(neargene) %>% summarise(SNP_annotate_neargene = SNP[which.min(pvalue)] )
+  don <- gwasResults %>%
+
+    # Compute chromosome size
+    group_by(CHR) %>%
+    summarise(chr_len=max(BP)) %>%
+
+    # Calculate cumulative position of each chromosome
+    mutate(tot=cumsum(chr_len)-chr_len) %>%
+    select(-chr_len) %>%
+
+    # Add this info to the initial dataset
+    left_join(gwasResults, ., by=c("CHR"="CHR")) %>%
+
+    # Add a cumulative position of each SNP
+    arrange(CHR, BP) %>%
+    mutate( chromosome=BP+tot) %>%
+
+    # Add highlight and annotation information
+    mutate(is_draw = ifelse(SNP %in% PCO_trans_uniq$SNP, "yes", "no") ) %>%
+    mutate(is_eQTL = ifelse(SNP %in% signals_eQTL, "yes", "no") ) %>%
+    mutate(is_sQTL = ifelse(SNP %in% signals_sQTL, "yes", "no") ) %>%
+    mutate(is_annotate_neargene = ifelse(SNP %in% anno_dat$SNP_annotate_neargene, "yes", "no"))
+
+  axisdf <- don %>% group_by(CHR) %>% summarize(center=( max(chromosome) + min(chromosome) ) / 2 )
+  label = PCO_trans_uniq[match(don[don$is_annotate_neargene=="yes", "SNP"], PCO_trans_uniq$SNP), "neargene"]
+  label[label=="NA"] = ""
+
+  manhattan_plot <- ggplot(don, aes(x=chromosome, y=-log10(P))) +
+
+    # Show all points
+    geom_point( aes(color=as.factor(CHR)), alpha=0.8, size=1.3) +
+    scale_color_manual(values = rep(c("grey", "grey"), 22 )) +
+
+    # custom X axis:
+    scale_x_continuous( label = axisdf$CHR, breaks= axisdf$center ) +
+    scale_y_continuous(expand = c(0, 0), limits = c(5, 18.5) ) +     # remove space between plot area and x axis
+    # Add highlighted points
+    geom_point(data=subset(don, is_eQTL=="yes" & is_sQTL=="no"), color="indianred2", shape=19, alpha = 0.7) +
+    geom_point(data=subset(don, is_eQTL=="no" & is_sQTL=="yes"), color="dodgerblue1", shape=19, alpha = 0.5) +
+    geom_point(data=subset(don, is_eQTL=="yes" & is_sQTL=="yes"), color="plum3", shape=19, alpha = 0.7) +
+
+    # Add label using ggrepel to avoid overlapping
+    geom_label_repel( data=subset(don, is_annotate_neargene=="yes"), aes(label=label), segment.colour="grey", size=3, fill = NA) +
+
+    # Custom the theme:
+    theme_bw() +
+    theme(
+      text = element_text(size=10),
+      legend.position="none",
+      panel.border = element_blank(),
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor.x = element_blank()
+    )
+  ggsave("manhattan_coding.png", manhattan_plot)
+  system("bash ~/imgcat manhattan_coding.png")
+
+  #ggsave("manhattan.png", manhattan_plot)
+  #system("bash ~/imgcat manhattan.png")
 
 }
 
 
 
+
+
+########## miscellanous ###########
+
 # prepare annotation files
 {
   require(data.table)
 
-  PCO_trans_indep = read.table("indep.signals.chr.module.perm10.txt", stringsAsFactors = FALSE, col.names = "SNP")
+
+  PCO_trans_indep = read.table(file_indep_signal, stringsAsFactors = FALSE, col.names = "SNP")
   PCO_trans_indep$PCO = PCO_trans_indep$SNP; PCO_trans_indep$elife = "no"; PCO_trans_indep$both = "no"
-  PCO_trans_indep[PCO_trans_indep$SNP=="3:56849749", c("elife", "both")] = "3:56849749"
+  #PCO_trans_indep[PCO_trans_indep$SNP=="3:56849749", c("elife", "both")] = "3:56849749"
   #PCO_trans_indep[PCO_trans_indep$SNP=="9:126985334", c("elife", "both")] = "9:126985334"
 
-  gene_pos = fread(paste0("/project2/xuanyao/data/mappability/gencode.v19.annotation.table.txt"),
-                        sep = '\t', header = T)
+  gene_pos = fread(paste0(file_annotation),
+                   sep = '\t', header = T)
   gene_pos = as.data.frame(gene_pos[gene_pos$Class %in% c('protein_coding', 'lincRNA'), ])
 
   PCO_trans_indep$neargene = "NA"
@@ -81,76 +198,3 @@
 
 
 }
-
-
-
-
-### plot manhattan plot
-cd ./scratch/manhattan_tmp/
-module load R/3.6.1
-R
-
-rm(list = ls())
-library(qqman)
-library(ggrepel)
-library(dplyr)
-
-gwasResults = readRDS("manhattan.rds")
-PCO_trans_uniq = read.table(file = "PCO.trans.uniq.txt", stringsAsFactors = FALSE, row.names = 1, header = T)
-PCO_trans_indep = read.table("PCO.trans.indep.txt", header = TRUE, stringsAsFactors = FALSE)
-PCO_trans_uniq[PCO_trans_uniq$pvalue==0, "pvalue"] = 1e-18 #pvalues<1e-17 is displayed as 0
-gwasResults$P = 1; gwasResults[match(PCO_trans_uniq$SNP, gwasResults$SNP), "P"] = PCO_trans_uniq$pvalue
-PCO_trans_indep[25, 'neargene']='IKZF1'
-{
-  don <- gwasResults %>%
-
-    # Compute chromosome size
-    group_by(CHR) %>%
-    summarise(chr_len=max(BP)) %>%
-
-    # Calculate cumulative position of each chromosome
-    mutate(tot=cumsum(chr_len)-chr_len) %>%
-    select(-chr_len) %>%
-
-    # Add this info to the initial dataset
-    left_join(gwasResults, ., by=c("CHR"="CHR")) %>%
-
-    # Add a cumulative position of each SNP
-    arrange(CHR, BP) %>%
-    mutate( chromosome=BP+tot) %>%
-
-    # Add highlight and annotation information
-    mutate( is_highlight=ifelse(SNP %in% PCO_trans_indep$SNP, "yes", "no")) %>%
-    mutate( is_annotate=ifelse(SNP %in% PCO_trans_indep$SNP, "yes", "no")) %>%
-    mutate( is_elife=ifelse(SNP %in% PCO_trans_indep$elife, "yes", "no") )
-
-
-  axisdf <- don %>% group_by(CHR) %>% summarize(center=( max(chromosome) + min(chromosome) ) / 2 )
-  label = PCO_trans_indep[match(don[don$is_annotate=="yes", "SNP"], PCO_trans_indep$SNP), "neargene"]
-
-  manhattan_plot <-   ggplot(don, aes(x=chromosome, y=-log10(P))) +
-
-    # Show all points
-    geom_point( aes(color=as.factor(CHR)), alpha=0.8, size=1.3) +
-    scale_color_manual(values = rep(c("grey", "grey"), 22 )) +
-
-    # custom X axis:
-    scale_x_continuous( label = axisdf$CHR, breaks= axisdf$center ) +
-    scale_y_continuous(expand = c(0, 0), limits = c(5, 20) ) +     # remove space between plot area and x axis
-    # Add highlighted points
-    geom_point(data=subset(don, is_highlight=="yes"), color="red", size=1.5) +
-    geom_point(data=subset(don, is_elife=="yes"), color="red", size=3, shape=2) +
-    # Add label using ggrepel to avoid overlapping
-    geom_label_repel( data=subset(don, is_annotate=="yes"), aes(label=label), size=3, fill = NA) +
-    # Custom the theme:
-    theme_bw() +
-    theme(
-      text = element_text(size=15),
-      legend.position="none",
-      panel.border = element_blank(),
-      panel.grid.major.x = element_blank(),
-      panel.grid.minor.x = element_blank()
-    )
-  ggsave("manhattan.pdf", manhattan_plot)
-}
-
