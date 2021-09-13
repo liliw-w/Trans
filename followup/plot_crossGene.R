97fb367a834aed8882557d8c39204a9aa97b9d59
ll <liliw1@uchicago.edu>
2021-09-13 01:01:10 -0500

    Write code in separate files
    This file is only for calculating and plotting cross map genes for individual genes.

--- followup/plot_crossGene.R
+++ followup/plot_crossGene.R
@@ -3,86 +3,50 @@ library(data.table)
 library(tidyverse)
 require(ggplot2)
 
+### File I/O
 file_gene_annotation = "/project2/xuanyao/data/mappability/gencode.v19.annotation.table.txt"
 file_cross_mappability = "/project2/xuanyao/data/mappability/hg19_gencode19_75merExon_36merUTR_2mismatch_cross_mappability_symmetric_mean.txt.gz"
 file.ex.var.regressed = "/scratch/midway2/liliw1/DGN_cross_map_filter/result/ex.var.regressed.rds"
-file.gene.meta = "/scratch/midway2/liliw1/DGN_cross_map_filter/result/gene.meta.txt"
-file.coexp.module = "/scratch/midway2/liliw1/DGN_cross_map_filter/result/coexp.module.rds"
-#file.z = "/scratch/midway2/liliw1/DGN_PCO.lambda.01_real/z/z.module68.chr22.txt.gz"
-file.p = NULL
-params1 = "/scratch/midway2/liliw1/DGN_PCO.lambda.01_real/script/"
-
+file_out = "crossGene_gene.rds"
 
 datExpr = readRDS(file.ex.var.regressed)
-gene.meta = read.table(file.gene.meta, sep = '\t', header = T, row.names = NULL, stringsAsFactors = F,  check.names = FALSE)
-coexp.module = readRDS(file.coexp.module)
 gene_annotation = fread(file_gene_annotation, header = TRUE)
 cross_map = fread(file_cross_mappability)
-# gene_all = gene_annotation[match(names(coexp.module$moduleLabels), gene_annotation$GeneSymbol), Geneid]
 
+gene_all = gene_annotation[match(colnames(datExpr), gene_annotation$GeneSymbol), Geneid]
 
-seq.module = c(1, 10, 30, 50, 100, 130, 157, 158)
-seq.chr = 22:1
-mat_crossGene_module = matrix(nrow = length(seq.module), ncol = 2,
-                    dimnames = list(paste0("module", seq.module), c("crossGene", "moduleSize") ))
-mat_crossGene_chr = matrix(nrow = length(seq.module), ncol = length(seq.chr),
-                       dimnames = list(paste0("module", seq.module), paste0("chr", seq.chr)))
+crossGene_gene = rep(NA, length(gene_all)); names(crossGene_gene) = gene_all
+for(k in 1:length(gene_all)){
+  x = gene_all[k]
+  crossGene_gene[k] = sum(cross_map$V1 %in% x | cross_map$V2 %in% x)
 
-for(module in seq.module){
-  gene_in_cluster = data.frame("gene" = names(coexp.module$moduleLabels[coexp.module$moduleLabels == module]), stringsAsFactors = F)
-  gene_w_pos = merge(gene_in_cluster, gene.meta)
-
-  module_Geneid = gene_annotation[match(gene_w_pos[, "gene"], gene_annotation$GeneSymbol), Geneid]
-  mat_crossGene_module[paste0("module", module), c("crossGene", "moduleSize")] =
-    c(length(unique(c(cross_map[cross_map$V1 %in% module_Geneid, V2], cross_map[cross_map$V2 %in% module_Geneid, V1]))),
-      length(module_Geneid))
-
-  for(chr in seq.chr){
-    gene_annotation_chr = gene_annotation[gene_annotation$Chromosome == paste0("chr", chr), ]
-
-    gene_trans = gene_w_pos[gene_w_pos[, "chr"] != paste0("chr", chr), "gene"]
-    module_Geneid = gene_annotation[match(gene_trans, gene_annotation$GeneSymbol), Geneid]
-    ind_cross_map = (cross_map$V1 %in% module_Geneid | cross_map$V2 %in% module_Geneid) & (cross_map$V1 %in% gene_annotation_chr$Geneid | cross_map$V2 %in% gene_annotation_chr$Geneid)
-    cross_map_compare = cross_map[ind_cross_map, ]
-    cross_map_gene_chr = unique(c(cross_map_compare[cross_map_compare$V1 %in% gene_annotation_chr$Geneid, V1], cross_map_compare[cross_map_compare$V2 %in% gene_annotation_chr$Geneid, V2]))
-
-    ind_crossmodule = cross_map$V1 %in% module_Geneid | cross_map$V2 %in% module_Geneid
-    mat_crossGene_chr[paste0("module", module), paste0("chr", chr)] =
-      length(unique(c(cross_map[ind_crossmodule & (cross_map$V1 %in% gene_annotation_chr$Geneid), V1], cross_map[ind_crossmodule & (cross_map$V2 %in% gene_annotation_chr$Geneid), V2] )))
-
-    cat("Chr ", chr, " is done!", "\n")
-  }
-
-  cat("Module ", module, " is done!", "\n")
+  cat(k, "-th gene searched!", "\n")
 }
-save(mat_crossGene_module, mat_crossGene_chr, file = "crossGene.RData")
+saveRDS(crossGene_gene, file_out)
 
 
-### Prepare plot data
-df_crossGene_module = cbind("Module" = rownames(mat_crossGene_module), as.data.frame(mat_crossGene_module, check.names = FALSE, stringsAsFactors = FALSE))
-df_crossGene_module$Module = factor(df_crossGene_module$Module, levels = paste0("module", sort(seq.module)))
+### Plot: #cross genes for each individual genes across all genes in DGN
+#crossGene_gene = readRDS("~/Downloads/crossGene_gene.rds")
 
-df_crossGene_chr = cbind("Module" = rownames(mat_crossGene_chr), as.data.frame(mat_crossGene_chr, check.names = FALSE, stringsAsFactors = FALSE)) %>%
-  pivot_longer(!Module, names_to = "Chr", values_to = "crossGene_chr")
-df_crossGene_chr$Chr = factor(df_crossGene_chr$Chr, levels = paste0("chr", 1:22))
-df_crossGene_chr$Module = factor(df_crossGene_chr$Module, levels = paste0("module", sort(seq.module)))
+#### Prepare data
+df_crossGene_gene = cbind("Gene" = names(crossGene_gene), as.data.frame(crossGene_gene, check.names = FALSE, stringsAsFactors = FALSE))
 
+group_breaks = sort(c(0, 1, 10, 100, 500, 1000, 2000, 3000, 4000, 5000, max(df_crossGene_gene$crossGene_gene)))
+x_lable = paste0(c("=", "=", rep("<=", length(group_breaks)-2)), group_breaks)
+df_crossGene_gene$group = factor(sapply(df_crossGene_gene$crossGene_gene, function(x)
+  group_breaks[x <=  group_breaks][1] ), levels = group_breaks)
 
-### Plot: #cross genes for each module
-fig_crossGene <- ggplot(df_crossGene_module, aes(x = Module)) +
-  geom_bar(aes(y = -moduleSize), stat="identity", fill = "grey65", position = position_nudge(y = -100)) +
-  geom_text(aes(y = -moduleSize-100, label = moduleSize)) +
-  geom_bar(aes(y = crossGene), stat="identity", fill = "grey51") +
-  labs(x = "Module", y = "Cross genes") +
+df_summary = df_crossGene_gene %>% group_by(group) %>% count()
+df_summary$percent = round(df_summary$n / sum(df_summary$n)*100, digits = 0)
+
+#### Plot
+fig_crossGene_gene <-ggplot(df_crossGene_gene, aes(x = group)) +
+  geom_bar() +
+  geom_text(data = df_summary, aes(x = group, y = n+150, label = paste0(n,"\n","(",percent,"%)")) ) +
+  scale_x_discrete(labels = x_lable) +
+  labs(x = "#cross genes", y = "#genes") +
   theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position="top")
-ggsave("plot/crossGene.png", fig_crossGene)
-system("bash ~/imgcat plot/crossGene.png")
 
-### Plot: #cross genes for each module on each chr
-fig_crossGene_chr <- ggplot(df_crossGene_chr, aes(Chr, crossGene_chr, group = Module, color = Module)) +
-  geom_line() +
-  labs(x = "Chr", y = "Cross genes on chr", color = NULL) +
-  theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position="top")
-ggsave("plot/crossGene_chr.png", fig_crossGene_chr)
-system("bash ~/imgcat plot/crossGene_chr.png")
+ggsave("plot/crossGene_gene.png", fig_crossGene_gene)
+system("bash ~/imgcat plot/crossGene_gene.png")
 
