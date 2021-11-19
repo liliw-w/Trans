75d3d32bad7b12b5fe5f0cb164b746eb3209110e
ll <liliw1@uchicago.edu>
2021-11-19 11:52:53 -0600

    Neater code
    reading multiple files using lapply instead of for iteration

--- followup/coloc_region_prop.R
+++ followup/coloc_region_prop.R
@@ -10,55 +10,41 @@ pp4Thre = 0.75
 pvalThre = 1e-8
 nsnpsThre = 5
 gwasPhenocode_seq = c(30080, 30090, 30100, 30110, 30010, 30020, 30030, 30040, 30050, 30060, 30070, 30270, 30240, 30250, 30260, 30280, 30290, 30300, 30000, 30120, 30130, 30140, 30150, 30160, 30180, 30190, 30200, 30210, 30220)
+
 dirPlot = "/scratch/midway2/liliw1/coloc/plot/"
 file_plot = "all.traits.coloc.region.summary.png"
-
-file_qtlColocReg = "/scratch/midway2/liliw1/coloc/data/qtlColocReg.txt.gz"
-file_trait_info = "/scratch/midway2/liliw1/coloc/ukbb_blood_traits.csv"
-qtlColocReg = fread(file_qtlColocReg, header = TRUE)
-trait_info = fread(file_trait_info, sep = ",", header = TRUE)
+file_res_coloc_reg_prop = "/scratch/midway2/liliw1/coloc/data/coloc_region_prop.txt"
 
 
 ########## loop over all traits ##########
-resPlot = NULL
-for (gwasPhenocode in gwasPhenocode_seq) {
+########## number of regions & coloc regions, proportion of coloc regions ##########
+file_resColoc = paste0("/scratch/midway2/liliw1/coloc/data/pheno", gwasPhenocode_seq, ".resColoc.txt.gz")
+resColoc_all = rbindlist(lapply(file_resColoc, fread, header = TRUE), use.names = TRUE)
 
-  ########## files and read files ##########
-  file_resColoc = paste0("/scratch/midway2/liliw1/coloc/data/pheno", gwasPhenocode, ".resColoc.txt.gz")
-  resColoc = fread(file_resColoc, header = TRUE)
-
-  ########## coloc regions proportion ##########
-  resColoc = qtlColocReg %>% select(c(Signal, Pval)) %>% right_join(y = resColoc, by = c("Signal" = "Region"))
-
-  nRegion = nrow(resColoc)
-  nRegionColoc = sum(resColoc$PP.H4.abf > pp4Thre & resColoc$nsnps >= nsnpsThre)
-
-  nRegionPval = sum(resColoc$Pval <= pvalThre)
-  nRegionPvalColoc = sum(resColoc$Pval <= pvalThre & resColoc$PP.H4.abf > pp4Thre & resColoc$nsnps >= nsnpsThre)
+res_coloc_reg_prop = resColoc_all %>%
+  group_by(Phenocode, trait) %>%
+  summarise(nRegion = n(),
+            nRegionColoc = sum(PP.H4.abf > pp4Thre & nsnps >= nsnpsThre),
+            nRegionPval = sum(Pval <= pvalThre),
+            nRegionPvalColoc = sum(Pval <= pvalThre & PP.H4.abf > pp4Thre & nsnps >= nsnpsThre)) %>%
+  ungroup()
+res_coloc_reg_prop = res_coloc_reg_prop %>% mutate(propColoc = nRegionColoc/nRegion, propPvalColoc = nRegionPvalColoc/nRegionPval)
 
 
-  ########## write results ##########
-  resPlot = rbind(resPlot, data.table("Phenocode" = gwasPhenocode,
-                                      "nRegion" = nRegion, "nRegionColoc" = nRegionColoc,
-                                      "nRegionPval" = nRegionPval, "nRegionPvalColoc" = nRegionPvalColoc))
-
-  cat("Trait", gwasPhenocode, "is done! \n")
-}
-
-########## colocalized regions proportion ##########
-resPlot = resPlot %>% mutate(propColoc = nRegionColoc/nRegion, propPvalColoc = nRegionPvalColoc/nRegionPval)
-# add trait info
-resPlot = trait_info %>% select(c("GWAS ID", "Trait Abbreviation")) %>%
-  right_join(y = resPlot, by = c("GWAS ID" = "Phenocode")) %>%
-  rename("Phenocode" = "GWAS ID", "trait" = "Trait Abbreviation")
+########## save results ##########
+fwrite(res_coloc_reg_prop, file_res_coloc_reg_prop, quote = FALSE, sep = "\t")
 
 
-########## visualize the proportions ##########
+
+######################################################
+########## 2. visualize the proportions ##########
+######################################################
+
 # order the traits based on the number of corresponding regions
-resPlot$trait = with(resPlot, reorder(trait, -nRegion))
+res_coloc_reg_prop$trait = with(res_coloc_reg_prop, reorder(trait, -nRegion))
 
 # figure 1: draw bar plot on number of reigons
-dat_fig_bar_prop = resPlot %>%
+dat_fig_bar_prop = res_coloc_reg_prop %>%
   pivot_longer(c(nRegion, nRegionPval), names_to = "regionType", values_to = "n") %>%
   pivot_longer(c(nRegionColoc, nRegionPvalColoc), names_to = "regionTypeColoc", values_to = "nColoc")
 
@@ -72,7 +58,7 @@ fig_bar_prop <- ggplot(dat_fig_bar_prop, aes(x = trait, y = n, fill = regionType
                      legend.position = "top")
 
 # figure 2: draw line plot on the colocalized region proportion
-dat_fig_line_prop = resPlot %>% select(c(trait, propColoc, propPvalColoc)) %>%
+dat_fig_line_prop = res_coloc_reg_prop %>% select(c(trait, propColoc, propPvalColoc)) %>%
   pivot_longer(c(propColoc, propPvalColoc), names_to = "Type", values_to = "proportion")
 
 fig_line_prop <- ggplot(dat_fig_line_prop, aes(x = trait, y = proportion, group = Type, color = Type)) +
