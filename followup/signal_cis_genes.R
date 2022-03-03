ab5a33cb9bbcb6b480f2c596f32f5aec157ee441
liliw-w <liliw.723@gmail.com>
2022-03-03 11:12:25 -0600

    change cis region distance

--- followup/signal_cis_genes.R
+++ followup/signal_cis_genes.R
@@ -5,6 +5,7 @@ library(tidyverse)
 file.qtl = 'FDR/signals.chr.module.perm10.fdr10.txt'
 file.gene.meta = '/project2/xuanyao/data/mappability/gencode.v19.annotation.table.txt'
 file_signal_cis_genes = 'postanalysis/signal_cis_genes.txt'
+dis_cis = 1e+6
 
 qtl = fread(file.qtl, header = FALSE, col.names = c("signal", "p", "q"))
 gene.meta = fread(file.gene.meta, header = TRUE)
@@ -26,7 +27,7 @@ cis.gene.meta = sapply(1:nrow(qtl), function(x){
   tmp_cis.gene.meta = gene.meta %>%
     filter(chr %in% tmp_qtl$chr) %>%
     mutate("dis" = abs(tmp_qtl$pos - Start)) %>%
-    filter(dis < 5e+4) %>%
+    filter(dis < dis_cis/2) %>%
     arrange(dis)
   c(
     tmp_cis.gene.meta$GeneSymbol[1],
