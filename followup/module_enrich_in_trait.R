0e40b92fcc5516198907aa0957bf2ed57b292b6e
liliw-w <liliw.723@gmail.com>
2022-06-08 11:20:40 -0500

    initial code

---
+++ followup/module_enrich_in_trait.R
@@ -0,0 +1,21 @@
+###############################################################
+########### if a colocalized trans-eQTL gene module ###########
+########### shows significant heritability enrichment in the colocalized complex trait ###########
+###############################################################
+rm(list = ls())
+library(data.table)
+library(tidyverse)
+
+# trait
+
+# coloc module
+
+# extract genes
+## keep just the trans genes
+
+# add buffer region
+
+# combine genes
+
+# LDSC input
+
