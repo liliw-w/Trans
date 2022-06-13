4144e9e7499f0d07e332bb90060215f05365e7c2
liliw-w <liliw.723@gmail.com>
2022-06-13 13:07:22 -0500

    assemble par h2 file across modules for a trait

---
+++ ldsc/6_1_assemble_par_h2_trait.R
@@ -0,0 +1,39 @@
+###############################################################
+########### assemble par h2 file across modules for a trait ###########
+###############################################################
+rm(list = ls())
+library(data.table)
+library(tidyverse)
+
+
+# paras and I/O -----
+gwasPhenocode <- '28067908_cd'
+
+
+files_par_h2 <- list.files('h2_enrich_par', paste0(gwasPhenocode, "_M\\d+", "_baseline.results"), full.names = TRUE)
+file_gwas_trait_info <- "/project2/xuanyao/llw/GWAS/72_traits_GWAS.csv"
+
+file_out <- paste0('h2_enrich_par/T_', gwasPhenocode, '_all_modules.results')
+
+
+# read data -----
+par_h2 <- map_dfr(
+  files_par_h2,
+  ~fread(cmd = paste("sed -n -e 1p -e 99p", .x), sep = "\t")
+)
+if(!all(par_h2$Category == "L2_1")) stop("Not all extracted rows are from custom annotation.")
+
+gwas_trait_info <- fread(file_gwas_trait_info, sep = ",", header = TRUE)
+
+
+# re-arrange data -----
+# add annotation name
+par_h2 <- par_h2 %>%
+  mutate(Category = str_extract(basename(!!files_par_h2), paste0(!!gwasPhenocode, "_M\\d+"))) %>%
+  separate(Category, c("trait_id", "trait", "module"), sep = "_", remove = FALSE, convert = TRUE) %>%
+  separate(module, c(NA, "module"), sep = "M", convert = TRUE)
+
+
+# output -----
+fwrite(par_h2, file_out, quote = FALSE, sep = "\t")
+
