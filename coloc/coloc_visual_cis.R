fa6e753a2fe7ae616b0275481c22dcef923eaba1
liliw-w <liliw.723@gmail.com>
2022-07-01 12:02:27 -0500

    refine code

--- coloc/coloc_visual_cis.R
+++ coloc/coloc_visual_cis.R
@@ -8,14 +8,12 @@ library(cowplot)
 
 
 ## output files
-dir_gwas = '/scratch/midway2/liliw1/coloc_cis_ARHGEF3/'
-setwd(dir_gwas)
-
-file_qtlColocReg_gwas = "qtlColocReg_gwas.txt.gz"
-file_gwasColocReg = "gwasColocReg.txt.gz"
-file_resColoc_all = "resColoc.txt.gz"
+file_qtlColocReg_gwas = "/project2/xuanyao/llw/coloc/cis/cis_e/data/qtlColocReg_gwas.txt.gz"
+file_gwasColocReg = "/project2/xuanyao/llw/coloc/cis/cis_e/data/gwasColocReg.txt.gz"
+file_resColoc_all = "/project2/xuanyao/llw/coloc/cis/cis_e/data/resColoc.txt.gz"
 
 pp4Thre = 0.75
+nsnpsThre = 5
 popColoc = "EUR"
 refGenom = 'hg19'
 
@@ -25,10 +23,14 @@ qtlColocReg_gwas = fread(file_qtlColocReg_gwas)
 gwasColocReg = fread(file_gwasColocReg)
 resColoc_all = fread(file_resColoc_all)
 
+dir.create(file.path("colocVisData"), recursive = TRUE, showWarnings = FALSE)
+dir.create(file.path("plot"), recursive = TRUE, showWarnings = FALSE)
+
 
 ########## only retain regions with high pp4 ##########
 #removeReg = c("module4:12:8142264")
 #resColoc = resColoc %>% filter(PP.H4.abf > pp4Thre)
+resColoc_all = filter(resColoc_all, PP.H4.abf > pp4Thre & nsnps >= nsnpsThre)
 
 
 ## remove SNPs without rsid, as locuscomparer requires rsid
@@ -37,9 +39,9 @@ gwasColocReg$rsid <- qtlColocReg_gwas[match(gwasColocReg$SNP_ID, qtlColocReg_gwa
 gwasColocReg = gwasColocReg[gwasColocReg$rsid != "", ]
 
 
-gwasPhenocode_seq = unique(resColoc_all$trait)
-gwas_trait_seq = unique(resColoc_all$trait)
-n_gwas = length(unique(resColoc_all$trait))
+gwasPhenocode_seq = unique(resColoc_all$gene)
+gwas_trait_seq = unique(resColoc_all$gene)
+n_gwas = length(unique(resColoc_all$gene))
 
 
 # i <- 57, 196, 264, 272, 396:398 ??? "There must be one and only one chromosome."
@@ -47,16 +49,16 @@ for(i in 1:n_gwas){
   gwasPhenocode = gwasPhenocode_seq[i]
   gwas_trait = gwas_trait_seq[i]
   
-  dir.create(file.path("colocVisData"), recursive = TRUE, showWarnings = FALSE)
-  dir.create(file.path("plot"), recursive = TRUE, showWarnings = FALSE)
 
 
-  resColoc <- resColoc_all %>% filter(trait == gwasPhenocode)
+  resColoc <- resColoc_all %>% filter(gene == gwasPhenocode)
   
   ########## Visualize each region & save ##########
   nRegion = length(resColoc$Region)
   
   for(reg in resColoc$Region){
+    M = strsplit(reg, ":") %>% unlist() %>% .[1] %>% strsplit(., "module") %>% unlist() %>% .[2] %>% paste("Module", .)
+    
     tmpresColoc = resColoc %>% filter(Region == reg)
     tmpqtlColocReg_gwas = qtlColocReg_gwas %>% filter(Region == reg)
     tmpgwasColocReg = gwasColocReg %>%
@@ -72,22 +74,27 @@ for(i in 1:n_gwas){
     fwrite(tmpgwasColocReg %>% select(rsid, npval) %>% rename(pval = npval),
            file_gwasVis, quote = FALSE, sep = "\t")
     
-    figTitle = paste0("Region: ", tmpresColoc$Region,
-                      "; Gene: ", gwasPhenocode,
-                      "; #SNPs: ", tmpresColoc$nsnps,
-                      "; PP.H4.abf: ", format(tmpresColoc$PP.H4.abf, digits = 4))
-    fig <- locuscompare(in_fn1 = file_gwasVis, in_fn2 = file_qtlVis,
-                        title1 = 'Gene', title2 = 'QTL',
+    figTitle = paste0("Region:", tmpresColoc$Region,
+                      "; Gene:", gwasPhenocode,
+                      "\n#SNPs:", tmpresColoc$nsnps,
+                      "; PP.H4.abf:", format(tmpresColoc$PP.H4.abf, digits = 4))
+    basic_plt <- locuscompare(in_fn1 = file_gwasVis, in_fn2 = file_qtlVis,
+                        title1 = gwasPhenocode, title2 = M,
                         population = popColoc, genome = refGenom)
     
-    fig <- fig + labs(title = figTitle) + theme(plot.title = element_text(hjust = 0.5, size = 12))
+    fig <- basic_plt +
+      labs(title = figTitle) +
+      theme(
+        plot.title = element_text(hjust = 0.5, size = 10)
+      )
     
-    ggsave(paste0(reg, ".", gwasPhenocode, ".reg.coloc.png"), fig, path = "plot/", width = 6, height = 6)
+    saveRDS(fig, paste0("plot/", reg, ".", gwasPhenocode, ".reg.coloc.rds"))
+    ggsave(paste0("plot/", reg, ".", gwasPhenocode, ".reg.coloc.pdf"), fig, width = 4, height = 3.5)
     
     ### in progress
     cat(grep(reg, resColoc$Region, fixed = TRUE),
-        "-th region:", reg, "(out of", nRegion, "regions)", "is done!", "\n")
-    cat("Gene: ", gwasPhenocode, '\n')
+        "-th region:", reg, "(out of", nRegion, "regions)", "is done! \n",
+        "Gene: ", gwasPhenocode, '\n')
   }
 }
 
@@ -104,7 +111,7 @@ res_coloc_reg_prop = resColoc_all %>%
 res_coloc_reg_prop <- resColoc_all %>%
   filter(PP.H4.abf > pp4Thre & nsnps >= nsnpsThre) %>%
   group_by(Region) %>%
-  summarise(gene = paste(trait, collapse = ";") ) %>%
+  summarise(gene = paste(gene, collapse = ";") ) %>%
   right_join(res_coloc_reg_prop, by = "Region") %>%
   arrange(desc(nGeneColoc), desc(nGene) )
 
@@ -112,11 +119,6 @@ res_coloc_reg_prop = res_coloc_reg_prop %>%
   mutate(propColoc = nGeneColoc/nGene )
 
 
-fwrite(res_coloc_reg_prop,  paste0("coloc_region_prop_pvalThre-", pvalThre, ".txt"),
+fwrite(res_coloc_reg_prop,  paste0("data/coloc_region_prop_pvalThre-", pvalThre, ".txt"),
        quote = FALSE, sep = "\t")
-
-
-
-
-
 
