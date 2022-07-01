52cbe865ef0973aed89b3c5504253533d80a23c1
liliw-w <liliw.723@gmail.com>
2022-07-01 11:57:04 -0500

    rename files

--- plot/plot_module_size.R
+++ plot/plot_module_size.R
@@ -1,69 +1,52 @@
+######################################################
+#################### Plot #modules v.s. module size ####################
+######################################################
 rm(list = ls())
-require(ggplot2)
-require(data.table)
-require(dplyr)
+library(data.table)
+library(tidyverse)
 
-# File name
-file_coexp_module = "/scratch/midway2/liliw1/DGN_cross_map_filter/result/coexp.module.rds"
-file_gene_annotation = "/project2/xuanyao/data/mappability/gencode.v19.annotation.table.txt"
-file_cross_mappability = "/project2/xuanyao/data/mappability/hg19_gencode19_75merExon_36merUTR_2mismatch_cross_mappability_symmetric_mean.txt.gz"
-
-# Load data from files
-coexp_module = readRDS(file_coexp_module)$moduleLabels
-gene_annotation = fread(file_gene_annotation, header = TRUE)
-cross_map = fread(file_cross_mappability)
-
-# DF for each gene
-df_module = data.frame("gene" = names(coexp_module),
-                       "module" = coexp_module,
-                       "if_cross_map" = names(coexp_module) %in% gene_annotation[match(unique(c(cross_map$V1, cross_map$V2)), gene_annotation$Geneid), GeneSymbol],
-                       "num_cross_map_within_module" = NA,
-                       check.names = FALSE, stringsAsFactors = FALSE)
-### df_module %>% group_by(module) %>% summarise(module_size = n(), num_cross_map = sum(if_cross_map))
-
-# Plot
-fig <- ggplot(df_module) + geom_bar(aes(module, fill = if_cross_map)) +
-  labs(x = "module", y = "size", fill = "if_cross_map") +
-  theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position="top") +
-  scale_fill_manual(values=c("dodgerblue2", "tomato3"))
-
-ggsave("plot_module_size.png", fig)
-system("bash ~/imgcat plot_module_size.png")
+file_coexp_module <- "/project2/xuanyao/llw/DGN_no_filter_on_mappability/result/coexp.module.rds"
+coexp_module <- readRDS(file_coexp_module)$moduleLabels
 
 
-# Count genes with cross-mappable genes within each module
-for (i in max(df_module$module):0) {
-  K = sum(df_module$module==i)
-  gene_names = df_module[df_module$module==i, "gene"]
-  gene_names_anno = gene_annotation[match(gene_names, gene_annotation$GeneSymbol), Geneid]
+### modules and their sizes
+df_module <- tibble("gene" = names(coexp_module),
+                       "module" = coexp_module)
+df_module_size <- df_module %>% group_by(module) %>% summarise(module_size = n())
 
-  cross_within_module = matrix(nrow = K, ncol = K,
-                               dimnames = list(gene_names_anno, gene_names_anno) )
-  diag(cross_within_module) = FALSE
-  for( j in 1:(K - 1) ){
-    target = rownames(cross_within_module)[j]
-    target_search = colnames(cross_within_module)[(j+1):K]
+### plot histogram of number of modules v.s. module size
+fig_modules_size <- ggplot(df_module_size) +
+  geom_histogram(aes(x = module_size, fill = after_stat(count)),
+                 binwidth = 10,
+                 color = "white") +
+  scale_fill_gradientn(colors = RColorBrewer::brewer.pal(8, "Blues")[8:4],
+                       na.value = "red") +
+  labs(x = "Module Size", y = "Number of Modules") +
+  theme_classic() +
+  theme(panel.grid.major.y = element_line(linetype = "dashed"),
+        panel.background = element_rect(fill = "white"),
+        
+        legend.position = "none",
+        legend.text = element_text(size = 10),
+        legend.title = element_text(size = 10, face = "bold"),
+        legend.background = element_rect(color = "black", linetype = "dashed"),
+        legend.key.size= unit(0.5, "cm"),
+        
+        axis.line = element_line(colour="black", size = 0.7),
+        axis.line.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        
+        plot.margin=unit(c(10,5,5,5),"mm"),
+        axis.text.x = element_text(colour="black", hjust=1, vjust = 1, size = 12),
+        axis.text.y = element_text(colour = "black", size = 12),
+        axis.title.x = element_text(vjust = -0.2, size=14),
+        axis.title.y = element_text(angle=90,vjust =2, size=14)
+  )
 
-    target_cross_ind = cross_map$V1 %in% target | cross_map$V2 %in% target
-    if(sum(target_cross_ind) == 0){
-      cross_within_module[j, (j+1):K] = FALSE
-      next
-    }
-    target_cross = cross_map[target_cross_ind, ]
-    cross_within_module[j, (j+1):K] = target_search %in% c(target_cross$V1, target_cross$V2)
-  }
+fig_modules_size
 
-  cross_within_module[lower.tri(cross_within_module)] = t(cross_within_module)[lower.tri(cross_within_module)]
-  df_module[df_module$module==i, "num_cross_map_within_module"] = rowSums(cross_within_module)
-  cat("Module: ", i, " is done!", "\n")
-}
-saveRDS(df_module, "df_module.rds")
+### save figure and plotting object for further editing
+saveRDS(fig_modules_size, 'module_size.rds')
 
-fig_cross_within_module <- ggplot(df_module) + geom_bar(aes(module, fill = ifelse(num_cross_map_within_module==0, FALSE, TRUE))) +
-  labs(x = "module", y = "size", fill = "if_cross_map_within_module") +
-  theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position="top") +
-  scale_fill_manual(values=c("dodgerblue2", "tomato3"))
-
-ggsave("plot_module_size_cross_within_module.png", fig_cross_within_module)
-system("bash ~/imgcat plot_module_size_cross_within_module.png")
+ggsave('module_size.pdf', fig_modules_size, width = 6, height = 5)
 
