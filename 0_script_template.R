b8ef08aab0135a3374acd33e9fa59ae9cd3bc533
liliw-w <liliw.723@gmail.com>
2022-08-22 13:42:44 -0500

    change script template

---
+++ 0_script_template.R
@@ -0,0 +1,21 @@
+##############################################
+###########  ###########
+##############################################
+rm(list = ls())
+library(data.table)
+library(tidyverse)
+
+
+# I/O & paras -----
+
+## output -----
+
+
+# read files -----
+
+
+# organize data -----
+
+
+# print out key message and write out -----
+
