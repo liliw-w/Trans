453ff982dcceb5137dabe28878c3066230cf414b
liliw-w <liliw.723@gmail.com>
2022-10-03 10:49:16 -0500

    merged coloc regions and prop for cis coloc

---
+++ coloc/cis/2_1_coloc_prop_cis_merged_comb.R
@@ -0,0 +1,43 @@
+############################################################
+########## Merged coloc regions & proportions for traits ##########
+############################################################
+rm(list = ls())
+library(data.table)
+library(tidyverse)
+
+
+# I/O & paras -----
+file_list_resColoc <- list.files(
+  "/project2/xuanyao/llw/coloc/cis",
+  "^coloc_reg_w_merged.txt$",
+  full.names = TRUE, recursive = TRUE
+)
+file_reg_merged <- "/project2/xuanyao/llw/coloc/ukbb_coloc_blood_traits/data/qtl_region_merged.txt"
+
+## output -----
+file_res_coloc_reg_prop <- '/project2/xuanyao/llw/coloc/cis/coloc_region_prop_merged_es.txt'
+
+
+# read files -----
+resColoc_all <- rbindlist(lapply(file_list_resColoc, fread, header = TRUE), use.names = TRUE)
+reg_merged <- fread(file_reg_merged, header = TRUE)
+
+
+# add coloc regions, unmerged and merged. also add coloc proportion -----
+res_coloc_reg_prop <- resColoc_all %>%
+  summarise(nRegionPval = n_distinct(!!reg_merged$Region),
+            nRegionPvalColoc = n_distinct(Region),
+            nRegionPvalMerg = n_distinct(!!reg_merged$merged_region),
+            nRegionPvalColocMerg = n_distinct(merged_region)) %>%
+  ungroup() %>%
+  mutate(propPvalColoc = nRegionPvalColoc/nRegionPval,
+         propPvalColocMerg = nRegionPvalColocMerg/nRegionPvalMerg) %>%
+  arrange(desc(propPvalColocMerg))
+
+
+fwrite(
+  res_coloc_reg_prop,
+  file = file_res_coloc_reg_prop,
+  quote = FALSE,
+  sep = '\t'
+)
