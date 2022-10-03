f77acf5c6dcbebc5b87a08c9063fafb90888556c
liliw-w <liliw.723@gmail.com>
2022-10-03 10:55:28 -0500

    standarize a few variables names

--- modules/9_1_signal_cis_genes.R
+++ modules/9_1_signal_cis_genes.R
@@ -7,18 +7,17 @@ library(tidyverse)
 
 
 # I/O & paras -----
-file.qtl <- 'postanalysis/signal_rm_infl_ratio_50.txt'
-file.gene.meta <- '/project2/xuanyao/data/mappability/gencode.v19.annotation.table.txt'
+file_qtl <- 'postanalysis/signal_rm_infl_ratio_50.txt'
+file_gene_meta <- '/project2/xuanyao/data/mappability/gencode.v19.annotation.table.txt'
 dis_cis <- 1e+6
 
 ## output -----
 file_signal_cis_genes <- 'postanalysis/signal_cis_genes_rm_infl_ratio_50.txt'
 
 
-
 # read files -----
-qtl <- fread(file.qtl, header = TRUE)
-gene.meta <- fread(file.gene.meta, header = TRUE)
+qtl <- fread(file_qtl, header = TRUE)
+gene_meta <- fread(file_gene_meta, header = TRUE)
 
 
 # organize data -----
@@ -32,32 +31,32 @@ gene.meta <- fread(file.gene.meta, header = TRUE)
 #        sep = ":", remove = FALSE)
 
 ## extract protein_coding, lincRNA, auto-chr genes -----
-gene.meta <- gene.meta %>%
+gene_meta <- gene_meta %>%
   filter(Class %in% c("protein_coding", "lincRNA") & Chromosome %in% paste0("chr",1:22)) %>%
   separate("Chromosome", c(NA, "chr"), sep = "chr", remove = FALSE)
 
 
 # nearest and cis genes of each signal, with distance -----
-cis.gene.meta <- sapply(1:nrow(qtl), function(x){
+cis_gene_meta <- sapply(1:nrow(qtl), function(x){
   tmp_qtl = qtl[x, ]
-  tmp_cis.gene.meta = gene.meta %>%
+  tmp_cis_gene_meta = gene_meta %>%
     filter(chr %in% tmp_qtl$SNPChr) %>%
     mutate("dis" = abs(tmp_qtl$SNPPos - Start)) %>%
     filter(dis < dis_cis/2) %>%
     arrange(dis)
   c(
-    tmp_cis.gene.meta$GeneSymbol[1],
-    tmp_cis.gene.meta$dis[1],
-    paste(tmp_cis.gene.meta$GeneSymbol, collapse = ";"),
-    paste(tmp_cis.gene.meta$dis, collapse = ";")
+    tmp_cis_gene_meta$GeneSymbol[1],
+    tmp_cis_gene_meta$dis[1],
+    paste(tmp_cis_gene_meta$GeneSymbol, collapse = ";"),
+    paste(tmp_cis_gene_meta$dis, collapse = ";")
   )
 })
-cis.gene.meta <- as.data.table(t(cis.gene.meta))
-colnames(cis.gene.meta) <- c("nearest_gene", "nearest_dis", "near_genes", "near_dis")
+cis_gene_meta <- as.data.table(t(cis_gene_meta))
+colnames(cis_gene_meta) <- c("nearest_gene", "nearest_dis", "near_genes", "near_dis")
 
 
 ## add cis gene info to signals -----
-qtl <- bind_cols(qtl, cis.gene.meta)
+qtl <- bind_cols(qtl, cis_gene_meta)
 
 
 
