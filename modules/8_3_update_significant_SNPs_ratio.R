bd7b499a7b55e2b6ca624557797de28b0f26bf5f
liliw-w <liliw.723@gmail.com>
2022-10-03 10:54:48 -0500

    change fdr level to determine signals

--- modules/8_3_update_significant_SNPs_ratio.R
+++ modules/8_3_update_significant_SNPs_ratio.R
@@ -9,10 +9,13 @@ library(tidyverse)
 
 
 # I/O & paras -----
-thre_p_z <- 1e-4
 ratio_thre <- 50
+thre_p_z <- 1e-4
+fdr_level <- 0.1
+n_snp <- 9918
 
-file_signal <- 'postanalysis/signal.txt'
+
+file_p_all <- 'FDR/p_all_1e5.rds'
 file_ratio_module_chr <- 'null_SNP/ratio_module_chr.txt'
 
 ## output -----
@@ -21,7 +24,7 @@ file_signal_uniq_rm_infl_ratio <- paste0('postanalysis/signal_uniq_rm_infl_ratio
 
 
 # read files -----
-signal <- fread(file_signal, header = TRUE)
+p_all <- readRDS(file_p_all)
 ratio_module_chr <- fread(file_ratio_module_chr, header = TRUE)
 
 
@@ -30,17 +33,26 @@ module_chr_use <- filter(ratio_module_chr, prop_dim_module > ratio_thre) %>%
   distinct(module, SNPChr, prop_dim_module)
 
 
+# determin p cutoff based on chosen modules -----
+p_thre <- fdr_level/n_snp/n_distinct(module_chr_use$module)
+
+
 # extract signals based on ratio cutoff -----
-signal <- inner_join(
-  signal,
-  module_chr_use,
-  by = c("module", "SNPChr")
-) %>%
+# extract signals -----
+signal <- filter(p_all, p <= p_thre) %>%
+  separate(module, c(NA, "module"), sep = "module", convert = TRUE) %>%
+  select(module, meta, p, SNP, SNPChr, SNPPos) %>%
+  arrange(module, SNPChr, SNPPos, p) %>%
+  inner_join(
+    module_chr_use,
+    by = c("module", "SNPChr")
+  ) %>%
   select(module, meta, p, SNP, SNPChr, SNPPos) %>%
   arrange(module, SNPChr, SNPPos, p)
 
 signal_uniq <- distinct(signal, SNP, .keep_all = TRUE) %>%
   select(SNP, SNPChr, SNPPos, meta)
+
 
 
 # print out key message or write out -----
