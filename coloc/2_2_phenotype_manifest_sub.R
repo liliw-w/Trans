33e51ab6f085bd7defccb492d07f2ee4395e9f88
liliw-w <liliw.723@gmail.com>
2022-10-03 17:37:16 -0500

    refine code strucutre

--- coloc/2_2_phenotype_manifest_sub.R
+++ coloc/2_2_phenotype_manifest_sub.R
@@ -1,42 +1,42 @@
+##############################################
 ############## 1. Extract specific traits from ukbb traits manifest file ###############
 ############## Based on resources: Traits from eQTLGen traits; Traits from Mu et al paper; Powerful (and interesting) ukbb GWASs ##############
 ############## 2. download the sum stat files extracted ##############
 ############## 3. Visualize the sample sizes ##############
-
+##############################################
+# load packages -----
 rm(list = ls())
 library(data.table)
 library(tidyverse)
-library(ggplot2)
 
-############## files and parameters
-file_pheno_manifest = "/project2/xuanyao/llw/GWAS/UKB_nealelab/phenotype_manifest.tsv"
-if_run_extract = FALSE
+# I/O & paras -----
+file_pheno_manifest <- "/project2/xuanyao/llw/GWAS/UKB_nealelab/phenotype_manifest.tsv"
+if_run_extract <- FALSE
 
-### download ukbb sum stat files
-if_run_wget_download = FALSE
-file_wget_download = "/scratch/midway2/liliw1/coloc/wget_download_cmd.txt"
-dir_download = "/project2/xuanyao/llw/GWAS/UKB_nealelab/"
+## download ukbb sum stat files
+if_run_wget_download <- FALSE
+file_wget_download <- "/scratch/midway2/liliw1/coloc/wget_download_cmd.txt"
+dir_download <- "/project2/xuanyao/llw/GWAS/UKB_nealelab/"
 
-### output files
-file_pheno_manifest_subset = "/scratch/midway2/liliw1/coloc/phenotype_manifest_sub.tsv"
-file_plot = "/scratch/midway2/liliw1/coloc/phenotype_sub_N.png"
+## output -----
+file_pheno_manifest_subset <- "/scratch/midway2/liliw1/coloc/phenotype_manifest_sub.tsv"
+file_plot <- "/scratch/midway2/liliw1/coloc/phenotype_sub_N.png"
 
 
-## read files
-pheno_manifest = fread(file_pheno_manifest)
+# read files -----
+pheno_manifest <- fread(file_pheno_manifest)
 colnames(pheno_manifest)
 
 
-############## Select GWAS traits
+# 1. Extract specific traits from ukbb traits manifest file -----
+## check trait of interest -----
 if(if_run_extract){
-  
-  ### 1. check trait of interest
-  trait_of_interest = "height"
+  trait_of_interest <- "height"
   grep(trait_of_interest, pheno_manifest$description, ignore.case = TRUE)
   View(pheno_manifest[grep(trait_of_interest, pheno_manifest$description, ignore.case = TRUE), ])
   
   
-  ### 2. powerful ukbb gwas (i.e. with large cases size of EUR)
+  ## powerful ukbb gwas (i.e. with large cases size of EUR)
   View(pheno_manifest %>%
          arrange(desc(n_cases_EUR)) %>%
          select(c(1:7, n_cases_EUR, n_controls_EUR)) %>%
@@ -45,8 +45,8 @@ if(if_run_extract){
 }
 
 
-############## phenocode of selected traits
-ukbb_trait_phenocode = c("30690", "30760",
+## phenocode of selected traits -----
+ukbb_trait_phenocode <- c("30690", "30760",
                          "21001", "23104",
                          "50",
                          "335",
@@ -65,36 +65,38 @@ ukbb_trait_phenocode = c("30690", "30760",
                          "278",
                          "300",
                          "185")
-### multiple categorical, which need to specify coding
-ukbb_categ_trait_phenocode = c("6152", "6152", "20126")
-ukbb_categ_trait_coding = c("8", "9", "0")
+## multiple categorical, which should be specified coding
+ukbb_categ_trait_phenocode <- c("6152", "6152", "20126")
+ukbb_categ_trait_coding <- c("8", "9", "0")
 
 
-############## Extract traits
-pheno_manifest_subset = pheno_manifest %>%
+## Extract traits -----
+pheno_manifest_subset <- pheno_manifest %>%
   filter(phenocode %in% ukbb_trait_phenocode |
            (phenocode %in% ukbb_categ_trait_phenocode &
               coding %in% ukbb_categ_trait_coding) ) %>%
   arrange(desc(n_cases_EUR))
 
-# modify the phenocode and description for "categorical" traits, to make them unique and clear
-ind_catg = pheno_manifest_subset$trait_type == "categorical"
+## modify the phenocode and description for "categorical" traits, to make them unique and clear
+ind_catg <- pheno_manifest_subset$trait_type == "categorical"
 
-pheno_manifest_subset = pheno_manifest_subset %>%
+pheno_manifest_subset <- pheno_manifest_subset %>%
   mutate(phenocode_uniq = phenocode,
          trait = str_replace_all(description, "\\s+", "-"))
 
-pheno_manifest_subset[ind_catg, c("phenocode_uniq", "trait")] =
+pheno_manifest_subset[ind_catg, c("phenocode_uniq", "trait")] <-
   with(pheno_manifest_subset[ind_catg,],
        data.table(paste(phenocode, coding, sep = "_"),
                   str_replace_all(coding_description, "\\s+", "-") )
        )
 
 
-## write out files
+## write out interested traits -----
 fwrite(pheno_manifest_subset, file_pheno_manifest_subset, quote = FALSE, sep = "\t")
 
-## download ukbb sum stat
+
+
+# 2. download the extracted ukbb sum stat files -----
 if(if_run_wget_download){
   fwrite(data.table(paste(pheno_manifest_subset$wget, "-P", dir_download)),
          file_wget_download,
@@ -104,22 +106,20 @@ if(if_run_wget_download){
 }
 
 
-############## Visualize the sample sizes (controls & cases) from extracted traits
-## data preparation
-plot_pheno_manifest_subset = pheno_manifest_subset %>%
+# 3. Visualize the sample sizes (controls & cases) from extracted traits -----
+plot_pheno_manifest_subset <- pheno_manifest_subset %>%
   select(c(trait_type, phenocode_uniq, coding,
            description, coding_description,
            n_cases_EUR, n_controls_EUR))
 
-# pivot the data for bar plot
-plot_pheno_manifest_subset = plot_pheno_manifest_subset %>%
+## pivot the data for bar plot -----
+plot_pheno_manifest_subset <- plot_pheno_manifest_subset %>%
   pivot_longer(cols = c("n_cases_EUR", "n_controls_EUR"),
                names_to = "n_type",
                values_to = "n") %>%
   mutate(n = replace_na(n, 0))
 
-## plot
-RColorBrewer::brewer.pal(10, "Paired") # check colors from a pallete
+## plot -----
 fig <- ggplot(plot_pheno_manifest_subset, aes(
   x = factor(phenocode_uniq,
              levels = unique(phenocode_uniq),
@@ -133,4 +133,7 @@ fig <- ggplot(plot_pheno_manifest_subset, aes(
                      panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
                      legend.position = "top")
 
+
+## print out key message or write out -----
 ggsave(file_plot, fig, width = 8, height = 4)
+
--- coloc/2_3_format_gwas_traits.R
+++ coloc/2_3_format_gwas_traits.R
@@ -1,22 +1,25 @@
+##############################################
 ########## Prepare coloc regions for gwas traits ##########
 ########## These traits are part of the 72 traits ##########
 ########## Focus on the 14 autoimmune diseases ##########
+##############################################
+# load packages -----
 rm(list = ls())
 library(data.table)
 library(tidyverse)
 
 
-########## Trait: RA, add beta, se columns ##########
-file_gwas = "/project2/xuanyao/llw/GWAS/pmid24390342_RA_GWASmeta_European_v2.txt.gz"
+# Trait: RA, add beta, se columns -----
+file_gwas <- "/project2/xuanyao/llw/GWAS/pmid24390342_RA_GWASmeta_European_v2.txt.gz"
 
 ## original gwas file
-gwas = fread(file = file_gwas)
+gwas <- fread(file = file_gwas)
 
 ## beta
-gwas$beta = log(gwas[["OR(A1)"]])
+gwas$beta <- log(gwas[["OR(A1)"]])
 
 ## se
-gwas$se = with(gwas, ( log(gwas[["OR_95%CIup"]]) - log(gwas[["OR_95%CIlow"]]) )/3.92 )
+gwas$se <- with(gwas, ( log(gwas[["OR_95%CIup"]]) - log(gwas[["OR_95%CIlow"]]) )/3.92 )
 
 ## save new formatted gwas file
 fwrite(gwas, file_gwas, quote = FALSE, sep = "\t")
@@ -24,54 +27,54 @@ fwrite(gwas, file_gwas, quote = FALSE, sep = "\t")
 
 
 
-########## Trait: IIBDGC - IBD, add beta ##########
-file_gwas = "/project2/xuanyao/llw/GWAS/pmid26192919_EUR.IBD.gwas_info03_filtered.assoc.gz"
+# Trait: IIBDGC - IBD, add beta -----
+file_gwas <- "/project2/xuanyao/llw/GWAS/pmid26192919_EUR.IBD.gwas_info03_filtered.assoc.gz"
 
 ## original gwas file
-gwas = fread(file = file_gwas)
+gwas <- fread(file = file_gwas)
 
 ## beta
-gwas$beta = log(gwas[["OR"]])
+gwas$beta <- log(gwas[["OR"]])
 
 ## save new formatted gwas file
 fwrite(gwas, file_gwas, quote = FALSE, sep = "\t")
 
 
-########## Trait: IIBDGC - CD, add beta ##########
-file_gwas = "/project2/xuanyao/llw/GWAS/pmid26192919_EUR.CD.gwas_info03_filtered.assoc.gz"
+# Trait: IIBDGC - CD, add beta -----
+file_gwas <- "/project2/xuanyao/llw/GWAS/pmid26192919_EUR.CD.gwas_info03_filtered.assoc.gz"
 
 ## original gwas file
-gwas = fread(file = file_gwas)
+gwas <- fread(file = file_gwas)
 
 ## beta
-gwas$beta = log(gwas[["OR"]])
+gwas$beta <- log(gwas[["OR"]])
 
 ## save new formatted gwas file
 fwrite(gwas, file_gwas, quote = FALSE, sep = "\t")
 
 
-########## Trait: IIBDGC - UC, add beta ##########
-file_gwas = "/project2/xuanyao/llw/GWAS/pmid26192919_EUR.UC.gwas_info03_filtered.assoc.gz"
+# Trait: IIBDGC - UC, add beta -----
+file_gwas <- "/project2/xuanyao/llw/GWAS/pmid26192919_EUR.UC.gwas_info03_filtered.assoc.gz"
 
 ## original gwas file
-gwas = fread(file = file_gwas)
+gwas <- fread(file = file_gwas)
 
 ## beta
-gwas$beta = log(gwas[["OR"]])
+gwas$beta <- log(gwas[["OR"]])
 
 ## save new formatted gwas file
 fwrite(gwas, file_gwas, quote = FALSE, sep = "\t")
 
 
 
-########## Trait: Lange IBD, add chr and pos ##########
-file_gwas = "/project2/xuanyao/llw/GWAS/pmid28067908_ibd_build37_59957_20161107.txt.gz"
+# Trait: Lange IBD, add chr and pos -----
+file_gwas <- "/project2/xuanyao/llw/GWAS/pmid28067908_ibd_build37_59957_20161107.txt.gz"
 
 ## original gwas file
-gwas = fread(file = file_gwas)
+gwas <- fread(file = file_gwas)
 
 ## chr and pos
-gwas = gwas %>%
+gwas <- gwas %>%
   separate(col = MarkerName, into = "SNP", sep = "_", remove = FALSE, convert = TRUE) %>%
   separate(col = SNP, into = c("Chr", "Pos"), sep = ":", remove = FALSE, convert = TRUE)
 
@@ -80,14 +83,14 @@ fwrite(gwas, file_gwas, quote = FALSE, sep = "\t")
 
 
 
-########## Trait: Lange CD, add chr and pos ##########
-file_gwas = "/project2/xuanyao/llw/GWAS/pmid28067908_cd_build37_40266_20161107.txt.gz"
+# Trait: Lange CD, add chr and pos -----
+file_gwas <- "/project2/xuanyao/llw/GWAS/pmid28067908_cd_build37_40266_20161107.txt.gz"
 
 ## original gwas file
-gwas = fread(file = file_gwas)
+gwas <- fread(file = file_gwas)
 
 ## chr and pos
-gwas = gwas %>%
+gwas <- gwas %>%
   separate(col = MarkerName, into = "SNP", sep = "_", remove = FALSE, convert = TRUE) %>%
   separate(col = SNP, into = c("Chr", "Pos"), sep = ":", remove = FALSE, convert = TRUE)
 
@@ -96,14 +99,14 @@ fwrite(gwas, file_gwas, quote = FALSE, sep = "\t")
 
 
 
-########## Trait: Lange UC, add chr and pos ##########
-file_gwas = "/project2/xuanyao/llw/GWAS/pmid28067908_uc_build37_45975_20161107.txt.gz"
+# Trait: Lange UC, add chr and pos -----
+file_gwas <- "/project2/xuanyao/llw/GWAS/pmid28067908_uc_build37_45975_20161107.txt.gz"
 
 ## original gwas file
-gwas = fread(file = file_gwas)
+gwas <- fread(file = file_gwas)
 
 ## chr and pos
-gwas = gwas %>%
+gwas <- gwas %>%
   separate(col = MarkerName, into = "SNP", sep = "_", remove = FALSE, convert = TRUE) %>%
   separate(col = SNP, into = c("Chr", "Pos"), sep = ":", remove = FALSE, convert = TRUE)
 
@@ -112,19 +115,19 @@ fwrite(gwas, file_gwas, quote = FALSE, sep = "\t")
 
 
 
-########## Trait: MS, add beta, se columns ##########
-file_gwas = "/project2/xuanyao/llw/GWAS/pmid31604244_MS_15_discovery_metav3.0.meta.gz"
+# Trait: MS, add beta, se columns -----
+file_gwas <- "/project2/xuanyao/llw/GWAS/pmid31604244_MS_15_discovery_metav3.0.meta.gz"
 
 ## original gwas file
-gwas = fread(file = file_gwas)
+gwas <- fread(file = file_gwas)
 
 ## beta
-gwas$beta = log(gwas[["OR"]])
+gwas$beta <- log(gwas[["OR"]])
 
 ## se
-gwas$abs_z = with(gwas, sqrt(qchisq(1 - P, 1)))
-gwas$se = with(gwas, abs(beta / abs_z) )
-gwas$abs_z = NULL
+gwas$abs_z <- with(gwas, sqrt(qchisq(1 - P, 1)))
+gwas$se <- with(gwas, abs(beta / abs_z) )
+gwas$abs_z <- NULL
 
 ## save new formatted gwas file
 fwrite(gwas, file_gwas, quote = FALSE, sep = "\t")
