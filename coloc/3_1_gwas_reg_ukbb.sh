c5b92cdc2f946f039d07e9e7db3be2b302e788ff
liliw-w <liliw.723@gmail.com>
2022-10-03 17:23:49 -0500

    refine code structure

---
+++ coloc/3_1_gwas_reg_ukbb.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH --time=5:00:00
+#SBATCH --partition=broadwl
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30G
+#SBATCH --output=gwas_coloc.o
+
+Rscript --no-restore --no-save ~/Trans/coloc/3_1_gwas_reg_ukbb.R
+
