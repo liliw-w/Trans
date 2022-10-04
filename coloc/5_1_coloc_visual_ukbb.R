70c3366bb32a47349561dca240785fd8d736afdc
liliw-w <liliw.723@gmail.com>
2022-10-04 11:12:21 -0500

    refine code

--- coloc/5_1_coloc_visual_ukbb.R
+++ coloc/5_1_coloc_visual_ukbb.R
@@ -10,18 +10,14 @@ library(locuscomparer)
 library(cowplot)
 
 
-file_pheno_manifest <- "/project2/xuanyao/llw/coloc/ukbb_coloc_blood_traits/ukbb_blood_traits.csv"
-dir_coloc <- "/project2/xuanyao/llw/coloc/ukbb_coloc_blood_traits/data"
+# loop over all blood traits -----
+file_blood_trait_info <- "/project2/xuanyao/llw/coloc/ukbb_coloc_blood_traits/ukbb_blood_traits.csv"
+trait_info <- fread(file_blood_trait_info, sep = ",", header = TRUE)
+pheno_seq <- trait_info$`GWAS ID`
+n_gwas <- length(pheno_seq)
 
-pheno_manifest <- fread(file_pheno_manifest)
-
-gwasPhenocode_seq <- pheno_manifest$`GWAS ID`
-gwas_trait_type_seq <- pheno_manifest$`GWAS Group`
-gwas_trait_seq <- pheno_manifest$`Trait Abbreviation`
-n_gwas <- nrow(pheno_manifest)
-
-which(pheno_manifest$`GWAS ID` == 30080)
-i <- 1
+gwasPhenocode <- pheno_seq[1]
+gwasPhenocode
 
 
 # I/O & paras -----
@@ -30,103 +26,101 @@ popColoc <- "EUR"
 refGenom <- 'hg19'
 
 
-for(i in 1:n_gwas){
-  gwasPhenocode <- gwasPhenocode_seq[i]
-  gwas_trait_type <- gwas_trait_type_seq[i]
-  gwas_trait <- gwas_trait_seq[i]
-  
-  dir_gwas <- file.path(dir_coloc,
-                       paste("ukbb",
-                             gwas_trait_type,
-                             gwasPhenocode,
-                             gwas_trait,
-                             sep = "_")
-  )
-  dir.create(file.path(dir_gwas, "colocVisData"), recursive = TRUE, showWarnings = FALSE)
-  dir.create(file.path(dir_gwas, "plot"), recursive = TRUE, showWarnings = FALSE)
-  
-  
-  # output -----
-  file_qtlColocReg_gwas <- "data/pheno30090.qtlColocReg_gwas.txt.gz"
-  file_gwasColocReg <- "data/pheno30090.gwasColocReg.txt.gz"
-  file_resColoc <- "data/pheno30090.resColoc.txt.gz"
-  
-  # read files -----
-  qtlColocReg_gwas <- fread(file_qtlColocReg_gwas)
-  gwasColocReg <- fread(file_gwasColocReg)
-  resColoc <- fread(file_resColoc)
-  
-  ## only retain regions with high pp4
-  resColoc <- resColoc %>% filter(PP.H4.abf > pp4Thre)
-  
-  
-  # Visualize each region -----
-  nRegion <- length(resColoc$Region)
-  
-  ## remove SNPs without rsid, as locuscomparer requires rsid
-  qtlColocReg_gwas <- qtlColocReg_gwas[qtlColocReg_gwas$rsid != "", ]
-  gwasColocReg <- gwasColocReg[gwasColocReg$rsid != "", ]
-  
-  for(reg in resColoc$Region){
-    tmpresColoc <- resColoc %>% filter(Region == reg)
-    tmpqtlColocReg_gwas <- qtlColocReg_gwas %>% filter(Region == reg)
-    tmpgwasColocReg <- gwasColocReg %>% filter(Region == reg)
-    
-    ## input files of locuscomparer -----
-    file_qtlVis <- paste0("colocVisData/", reg, ".qtl.reg.coloc.txt")
-    file_gwasVis <- paste0("colocVisData/", reg, ".gwas.reg.coloc.txt")
-    
-    fwrite(tmpqtlColocReg_gwas %>% select(rsid, Pval) %>% rename(pval = Pval),
-           file_qtlVis, quote = FALSE, sep = "\t")
-    fwrite(tmpgwasColocReg %>% select(rsid, starts_with("pval")) %>% rename(pval = pval_EUR),
-           file_gwasVis, quote = FALSE, sep = "\t")
-    
-    figTitle <- paste0("Region: ", tmpresColoc$Region, "; #SNPs: ", tmpresColoc$nsnps, "; PP.H4.abf: ", format(tmpresColoc$PP.H4.abf, digits = 4))
-    fig <- locuscompare(in_fn1 = file_gwasVis, in_fn2 = file_qtlVis,
-                        title1 = 'GWAS', title2 = 'QTL',
-                        population = popColoc, genome = refGenom)
-    
-    fig <- fig + labs(title = figTitle) + theme(plot.title = element_text(hjust = 0.5, size = 12))
-    
-    
-    ## save vis -----
-    ggsave(paste0(reg, ".reg.coloc.png"), fig, path = "plot/", width = 6, height = 6)
-    
-    cat(grep(reg, resColoc$Region, fixed = TRUE),
-        "-th region:", reg, "(out of", nRegion, "regions)", "is done!", "\n")
-  }
+## coloc directory for the trait
+dir_coloc_gwas <- list.files(
+  "/scratch/midway2/liliw1/coloc_MSigDB",
+  pattern = paste0("ukbb_continuous_", gwasPhenocode),
+  full.names = TRUE
+)
+dir.create(file.path(dir_coloc_gwas, "colocVisData"), recursive = TRUE, showWarnings = FALSE)
+dir.create(file.path(dir_coloc_gwas, "plot"), recursive = TRUE, showWarnings = FALSE)
+
+file_qtlColocReg_gwas <- paste(dir_coloc_gwas, "data/qtlColocReg_gwas.txt.gz", sep = '/')
+file_gwasColocReg <- paste(dir_coloc_gwas, "data/gwasColocReg.txt.gz", sep = '/')
+file_resColoc <- paste(dir_coloc_gwas, "data/resColoc.txt.gz", sep = '/')
+
+
+## output -----
+file_plt_coloc_sum <- paste(dir_coloc_gwas, "plot/coloc.region.summary.pdf", sep = '/')
+
+
+# read files -----
+qtlColocReg_gwas <- fread(file_qtlColocReg_gwas)
+gwasColocReg <- fread(file_gwasColocReg)
+resColoc <- fread(file_resColoc)
+
+
+## only retain regions with high pp4
+resColoc <- resColoc %>% filter(PP.H4.abf > pp4Thre)
+
+
+# Visualize each region -----
+nRegion <- length(resColoc$Region)
+
+## remove SNPs without rsid, as locuscomparer requires rsid
+qtlColocReg_gwas <- qtlColocReg_gwas[qtlColocReg_gwas$rsid != "", ]
+gwasColocReg <- gwasColocReg[gwasColocReg$rsid != "", ]
+
+for(reg in resColoc$Region){
+  file_qtlVis <- paste0(dir_coloc_gwas, "/colocVisData/", reg, ".qtl.reg.coloc.txt")
+  file_gwasVis <- paste0(dir_coloc_gwas, "/colocVisData/", reg, ".gwas.reg.coloc.txt")
+  file_plt_vis <- paste0(dir_coloc_gwas, "/plot/", reg, ".reg.coloc.pdf")
   
   
-  # Summary of the colocalized regions -----
-  resColoc <- resColoc %>% separate(col = Region, into = c("Module", "Chr", "Pos"), sep = ":", remove = FALSE, convert = TRUE)
-  resColoc$Module <- factor(resColoc$Module, levels = paste0("module", 1:200))
+  ## input files of locuscomparer -----
+  tmpresColoc <- resColoc %>% filter(Region == reg)
+  tmpqtlColocReg_gwas <- qtlColocReg_gwas %>% filter(Region == reg)
+  tmpgwasColocReg <- gwasColocReg %>% filter(Region == reg)
   
-  ## SNPs in each region -----
-  figNumofSnps <- ggplot(resColoc, aes(y = reorder(Region, nsnps), x = nsnps)) +
-    geom_bar(stat = "identity") +
-    labs(x = "#SNPs in region", y = "Region") +
-    theme_bw() + theme(axis.text.y = element_text(size = 7), panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+  fwrite(tmpqtlColocReg_gwas %>% select(rsid, Pval) %>% rename(pval = Pval),
+         file_qtlVis, quote = FALSE, sep = "\t")
+  fwrite(tmpgwasColocReg %>% select(rsid, starts_with("pval")) %>% rename(pval = pval_EUR),
+         file_gwasVis, quote = FALSE, sep = "\t")
   
-  ## number of regions for each module -----
-  figModCount <- ggplot(resColoc, aes(y = Module)) +
-    geom_bar(stat = "count") +
-    labs(x = "#regions") +
-    theme_bw() + theme(axis.text.y = element_text(size = 7), panel.grid.major = element_blank(), panel.grid.minor = element_blank())
   
-  ## number of regions for each chr -----
-  figChrCount <- ggplot(resColoc, aes(y = Chr)) +
-    geom_bar(stat = "count") +
-    labs(x = "#regions") +
-    theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-  
-  ## combine the plots -----
-  right_col <- plot_grid(figModCount, figChrCount, labels = c('B', "C"), ncol = 1)
-  fig <- plot_grid(figNumofSnps, right_col, labels = c('A', ''), ncol = 2)
+  ## locus vis -----
+  figTitle <- paste0("Region: ", tmpresColoc$Region, "; #SNPs: ", tmpresColoc$nsnps, "; PP.H4.abf: ", format(tmpresColoc$PP.H4.abf, digits = 4))
+  fig <- locuscompare(in_fn1 = file_gwasVis, in_fn2 = file_qtlVis,
+                      title1 = 'GWAS', title2 = 'QTL',
+                      population = popColoc, genome = refGenom)
+  fig <- fig + labs(title = figTitle) + theme(plot.title = element_text(hjust = 0.5, size = 12))
   
   
   ## save vis -----
-  ggsave("coloc.region.summary.png", fig, path = "plot/", width = 10, height = 10)
+  ggsave(file_plt_vis, fig, width = 4, height = 3)
   
-  cat(i, '-th out of', n_gwas, "GWASs is done. Trait is", gwas_trait, ".\n\n")
+  cat(grep(reg, resColoc$Region, fixed = TRUE),
+      "-th region:", reg, "(out of", nRegion, "regions)", "is done!", "\n")
 }
+
+
+# Summary of the colocalized regions -----
+resColoc <- resColoc %>% separate(col = Region, into = c("Module", "Chr", "Pos"), sep = ":", remove = FALSE, convert = TRUE)
+resColoc$Module <- factor(resColoc$Module, levels = paste0("module", 1:200))
+
+## SNPs in each region -----
+figNumofSnps <- ggplot(resColoc, aes(y = reorder(Region, nsnps), x = nsnps)) +
+  geom_bar(stat = "identity") +
+  labs(x = "#SNPs in region", y = "Region") +
+  theme_bw() + theme(axis.text.y = element_text(size = 7), panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
+## number of regions for each module -----
+figModCount <- ggplot(resColoc, aes(y = Module)) +
+  geom_bar(stat = "count") +
+  labs(x = "#regions") +
+  theme_bw() + theme(axis.text.y = element_text(size = 7), panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
+## number of regions for each chr -----
+figChrCount <- ggplot(resColoc, aes(y = Chr)) +
+  geom_bar(stat = "count") +
+  labs(x = "#regions") +
+  theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
+## combine the plots -----
+right_col <- plot_grid(figModCount, figChrCount, labels = c('B', "C"), ncol = 1)
+fig <- plot_grid(figNumofSnps, right_col, labels = c('A', ''), ncol = 2)
+
+
+## save vis -----
+ggsave(file_plt_coloc_sum, fig, width = 4, height = 5)
 
