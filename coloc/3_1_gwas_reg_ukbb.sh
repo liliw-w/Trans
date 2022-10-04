25aad040c65704fc3a75d10b99c7e8fac5997c8f
liliw-w <liliw.723@gmail.com>
2022-10-04 11:18:30 -0500

    increase memory

--- coloc/3_1_gwas_reg_ukbb.sh
+++ coloc/3_1_gwas_reg_ukbb.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=broadwl
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=30G
+#SBATCH --mem=40G
 #SBATCH --output=gwas_coloc.o
 
 Rscript --no-restore --no-save ~/Trans/coloc/3_1_gwas_reg_ukbb.R
