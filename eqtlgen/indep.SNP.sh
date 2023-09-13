21a18872de834bef8ebd17ea82e6d66416315410
Lili Wang <liliw1@midway3-0316.rcc.local>
2023-09-13 16:07:21 -0500

    delete redundant io if jobs fail

--- eqtlgen/indep.SNP.sh
+++ eqtlgen/indep.SNP.sh
@@ -1,6 +1,7 @@
 sig_uniq=$1
 sig_indp=$2
 dir_geno=/project2/xuanyao/llw/DGN_data/geno/
+dir_plink=/scratch/midway3/liliw1/conda_env/software/bin
 geno_prefix=chr
 geno_suffix=_QCed
 
@@ -10,15 +11,18 @@ geno_suffix=_QCed
 
 for chr in {1..22}
 do
-plink --bfile $dir_geno$geno_prefix$chr$geno_suffix \
+${dir_plink}/plink  \
+--bfile $dir_geno$geno_prefix$chr$geno_suffix \
 --extract $sig_uniq \
 --indep-pairwise 50 5 0.2 \
 --out indep.eigene.eqtls.$chr
 #echo chr$chr
-done
+done &&
 
-cat indep.eigene.eqtls.*.prune.in > in.txt
-cat indep.eigene.eqtls.*.prune.out > out.txt
-grep -vFf out.txt $sig_uniq | sort > $sig_indp
-rm indep.eigene.eqtls* in.txt out.txt
+cat indep.eigene.eqtls.*.prune.in > in.txt &&
+cat indep.eigene.eqtls.*.prune.out > out.txt &&
+grep -vFf out.txt $sig_uniq | sort > $sig_indp &&
+rm indep.eigene.eqtls* in.txt out.txt ||
+
+(echo "Error occurred..." && rm indep.eigene.eqtls*)
 
