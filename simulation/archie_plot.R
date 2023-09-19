8efcfedd6013455a9e7fd077b04f8332d03cb027
Lili Wang <liliw1@midway3-0316.rcc.local>
2023-09-19 15:53:08 -0500

    add more figures

--- simulation/archie_plot.R
+++ simulation/archie_plot.R
@@ -1,5 +1,5 @@
 ##############################################
-###########  ###########
+############ archie cc plot ############
 ##############################################
 # load packages -----
 rm(list = ls())
@@ -15,10 +15,11 @@ theme_set(
 if(interactive()){
   args <- scan(
     text = '
-    archie_cc_value.rds
-    archie_selected_gene.rds
-    plt_archie_cc_value.pdf
-    plt_archie_selected_gene.pdf
+    TRUE
+    archie_null_cc_value_z.rds
+    archie_null_selected_gene_z.rds
+    plt_archie_null_cc_value_z.pdf
+    plt_archie_null_selected_gene_z.pdf
     ',
     what = 'character'
   )
@@ -26,57 +27,94 @@ if(interactive()){
   args <- commandArgs(trailingOnly = TRUE)
 }
 
-file_cc_alt <- args[1]
-file_selected_gene_alt <- args[2]
+if_null <- as.logical(args[1])
+file_cc <- args[2]
+file_selected_gene <- args[3]
 
 ## output -----
-file_fig_cc <- args[3]
-file_fig_selected_gene <- args[4]
+file_fig_cc <- args[4]
+file_fig_selected_gene <- args[5]
 
 
 
 # read files -----
-cc_alt <- readRDS(file_cc_alt)
-selected_gene_alt <- readRDS(file_selected_gene_alt)
+cc <- readRDS(file_cc)
+selected_gene <- readRDS(file_selected_gene)
 
 
 # figures -----
-## distribution of cc value across models -----
-cc_alt[, 'archie', , , drop = TRUE] %>%
-  as_tibble(rownames = "model") %>%
-  pivot_longer(
-    cols = !model,
-    names_to = NULL, values_to = "cc_value"
-  ) %>%
-  ggplot() +
-  geom_boxplot(
-    aes(x = model, y = cc_value)
+if(if_null){
+  ## distribution of cc value across models -----
+  cc[,, drop = TRUE] %>%
+    enframe(name = NULL, value = "cc_value") %>%
+    ggplot() +
+    geom_boxplot(
+      aes(x = factor("Null"), y = cc_value),
+      outlier.shape = 4, outlier.size = 1, outlier.alpha = 0.2
+    ) +
+    labs(x = NULL)
+  
+  ggsave(
+    file_fig_cc,
+    height = 3, width = 2
   )
-
-ggsave(
-  file_fig_cc,
-  height = 3, width = 4
-)
-
-
-
-## distribution of number of selected genes across models -----
-apply(selected_gene_alt, c(3, 4), sum) %>%
-  t(.) %>%
-  as_tibble() %>%
-  pivot_longer(
-    cols = everything(),
-    names_to = "model", values_to = "num_gene"
-  ) %>%
-  ggplot() +
-  geom_boxplot(
-    aes(x = model, y = num_gene)
-  ) +
-  geom_hline(yintercept = 30, linetype = "dashed", color = "maroon") +
-  labs(x = "Model", y = "Number of selected genes")
-
-ggsave(
-  file_fig_selected_gene,
-  height = 3, width = 4
-)
+  
+  
+  ## distribution of number of selected genes across models -----
+  apply(selected_gene, 2, sum) %>%
+    enframe(name = NULL, value = "num_gene") %>%
+    ggplot() +
+    geom_boxplot(
+      aes(x = factor("Null"), y = num_gene),
+      outlier.shape = 4, outlier.size = 1, outlier.alpha = 0.2
+    ) +
+    geom_hline(yintercept = 30, linetype = "dashed", color = "maroon") +
+    labs(x = NULL, y = "Number of selected genes")
+  
+  ggsave(
+    file_fig_selected_gene,
+    height = 3, width = 2
+  )
+  
+  
+}else{
+  ## distribution of cc value across models -----
+  cc[, 'archie', , , drop = TRUE] %>%
+    as_tibble(rownames = "model") %>%
+    pivot_longer(
+      cols = !model,
+      names_to = NULL, values_to = "cc_value"
+    ) %>%
+    ggplot() +
+    geom_boxplot(
+      aes(x = model, y = cc_value)
+    )
+  
+  ggsave(
+    file_fig_cc,
+    height = 3, width = 4
+  )
+  
+  
+  ## distribution of number of selected genes across models -----
+  apply(selected_gene, c(3, 4), sum) %>%
+    t(.) %>%
+    as_tibble() %>%
+    pivot_longer(
+      cols = everything(),
+      names_to = "model", values_to = "num_gene"
+    ) %>%
+    ggplot() +
+    geom_boxplot(
+      aes(x = model, y = num_gene)
+    ) +
+    geom_hline(yintercept = 30, linetype = "dashed", color = "maroon") +
+    labs(x = "Model", y = "Number of selected genes")
+  
+  ggsave(
+    file_fig_selected_gene,
+    height = 3, width = 4
+  )
+  
+}
 
