90aa54763c83096545ab5982a98215a172a1f5f8
Lili Wang <liliw1@midway3-login4.rcc.local>
2024-02-06 17:32:09 -0600

    add annotations

--- followup/ld_clumping_pre.R
+++ followup/ld_clumping_pre.R
@@ -1,16 +1,25 @@
+##############################################
+########### ld clumping file prep ###########
+##############################################
+# load packages -----
+rm(list = ls())
+library(tidyverse)
+
+# I/O & paras -----
+file_qtl <- '/project2/xuanyao/llw/eQTLGen/DGN_est_Sigma/postanalysis/qtl_table.txt'
 # '/project2/xuanyao/llw/eQTLGen/DGN_est_Sigma/postanalysis/qtl_table.txt'
 # '/project2/xuanyao/llw/eQTLGen/MSigDB_est_Sigma/postanalysis/qtl_table.txt'
 
-
-file_qtl <- '/project2/xuanyao/llw/eQTLGen/DGN_est_Sigma/postanalysis/qtl_table.txt'
-
+## output -----
 file_unique_snp <- '/project2/xuanyao/llw/eQTLGen/DGN_est_Sigma/ld_clump/uniq_snp.txt'
 file_with_p <- '/project2/xuanyao/llw/eQTLGen/DGN_est_Sigma/ld_clump/ld_clump_esnp.txt'
 
 
+# read files -----
 qtl <- data.table::fread(file_qtl)
 
-# unique snps
+
+# unique snps -----
 distinct(qtl, SNP) %>% 
   data.table::fwrite(
     ., 
@@ -19,7 +28,7 @@ distinct(qtl, SNP) %>%
   )
 
 
-# snp/snp pair with p
+# snp/snp pair with p -----
 group_by(qtl, SNP) %>% 
   summarise(
     P = min(P)
@@ -30,6 +39,4 @@ group_by(qtl, SNP) %>%
     file = file_with_p,
     sep = '\t', quote = FALSE
   )
-
-
 
